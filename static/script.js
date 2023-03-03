decba4e66261e67a5331a6dcbbe142a5e5ccf1a6
YuriAlvarenga <alvarenga.yur@hotmail.com>
2023-03-03 05:00:58 -0300

    atualizado 03-03-2023

--- static/script.js
+++ static/script.js
@@ -6,3 +6,16 @@ menuMobile.addEventListener('click', ()=>{
     ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle('menu-nav-active')
 })
+
+/* Fecha o menu quando clica em algum item*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
